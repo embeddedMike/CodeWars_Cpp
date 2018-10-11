36594d2879610a64372779fe81149da27aa058be
Michal <209486.mb@gmail.com>
2018-10-11 22:27:30 +0200

    Add fibonacci solution
    Add class Fibonacci
    Add some tests for Fibonacci class methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,45 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include "../inc/main.hpp"
 
 
-int main()
-{
+int main() {
 
-    std::cout<<"Hello World"<<std::endl;
+  std::cout<<fibonacciNumbers(10)<<std::endl;
 
 }
 
 std::string spinWords(const std::string &str) {
-  return "spoko";
+
+  std::string result("");
+  std::string copy(str);
+  std::reverse(copy.begin(),copy.begin()+4);
+  result += copy.substr(0,4);  
+
+  return result;
+}
+
+int fibonacciNumbers(int n) {
+
+  int fib = 0;
+  int a = 1;
+  int b= 2;
+  
+  if(n == 0) {
+    return 0;
+  }
+  if(n == 1 || n == 2) {
+    return 1;
+  }
+  for(int i = 2; i < n-1; i++) {
+    fib = a + b;
+    a = b;
+    b = fib;
+  }
+  return fib;
+}
+
+int Fibonacci::fibonacciNumbers(int n) {
+  return 0;
 }
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -4,3 +4,16 @@
 TEST(StringTest, spinWords) {
     EXPECT_TRUE(3 == 3);
 }
+
+TEST(FibonacciTest, n_3) {
+  int fib = fibonacciNumbers(3);
+  EXPECT_TRUE(fib == 3);
+}
+TEST(FibonacciTest, n_5) {
+  int fib = fibonacciNumbers(5);
+  EXPECT_TRUE(fib == 5);
+}
+TEST(FibonacciTest, n_7) {
+  int fib = fibonacciNumbers(7);
+  EXPECT_TRUE(fib == 13);
+}
