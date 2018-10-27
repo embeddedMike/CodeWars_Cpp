64dc550f6c3e13d463abea6002bb4fda07667f40
Michal <209486.mb@gmail.com>
2018-10-27 23:10:28 +0200

    Modification on main.cpp and .travis.yml
    - add lpthreads on .travis.yml
    - remove some content from main.cpp

--- inc/main.hpp
+++ inc/main.hpp
@@ -1,13 +1,1 @@
 #pragma once
-std::string spinWords(const std::string &str);
-int fibonacciNumbers(int n);
-
-class Fibonacci {
-  int n_;
-
-  Fibonacci(int n)
-    : n_(n) {};
-
-  public:
-  int fibonacciNumbers(int n);
-};
