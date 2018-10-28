fdd57829b8906bbca2c5a2cbb483ba88b0dfb085
Michal <209486.mb@gmail.com>
2018-10-28 19:03:43 +0100

    Modification on test_main.cpp
    - Add main function

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -17,3 +17,17 @@ TEST_F(FibonacciTest, n_3) {
   //Then: EXPECT_TRUE(fib == 3);
 }
 }*/
+
+int main(int argc, char** argv)
+{
+  try {
+    testing::InitGoogleTest(&argc, argv);
+    auto code = RUN_ALL_TESTS();
+    return code;
+  }
+  catch (...)
+  {
+    return 0;
+  }
+  return 0;
+}
