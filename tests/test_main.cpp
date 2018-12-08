da3018e64be0dee33509c44805212527bcb21cb8
Michal <209486.mb@gmail.com>
2018-12-08 20:59:15 +0100

    Modification of .travis.yml

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,16 +1,9 @@
 #include <gtest/gtest.h>
-#include "../inc/main.hpp"
+//#include "../inc/main.hpp"
 
 int main(int argc, char** argv)
 {
-  try {
     testing::InitGoogleTest(&argc, argv);
-    auto code = RUN_ALL_TESTS();
-    return code;
-  }
-  catch (...)
-  {
-    return 0;
-  }
-  return 0;
+    return RUN_ALL_TESTS();
+    
 }
