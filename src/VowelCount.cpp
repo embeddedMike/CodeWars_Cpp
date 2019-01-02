de7023ab3f43235f446e440c31f08966faf83444
Michal <209486.mb@gmail.com>
2019-01-02 21:34:06 +0100

    Modification of VowelCount.cpp
    - Implementation of getCount method
    - Unit tests for method passed

--- src/VowelCount.cpp
+++ src/VowelCount.cpp
@@ -1,7 +1,16 @@
 #include "../inc/VowelCount.hpp"
 
 int VowelCount::getCount(const std::string& inputStr){
+  const std::string vowels = "aeio";
   int num_vowels = 0;
-  //your code here
+  
+  for(const auto item1 : inputStr) {
+    for(const auto item2 : vowels) {
+        if(item1 == item2) {
+          num_vowels++;
+        }
+    }
+  }
+  
   return num_vowels;
 }
