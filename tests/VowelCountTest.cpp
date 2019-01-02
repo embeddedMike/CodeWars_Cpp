7857901908c941a1271212580c2b3babbd008891
Michal <209486.mb@gmail.com>
2019-01-02 21:02:55 +0100

    Add VowelCountTest.cpp file
    - Define first test fixture getCountPass

---
+++ tests/VowelCountTest.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "../inc/VowelCount.hpp"
+
+struct VowelCountTest : public ::testing::Test {
+  VowelCount Vowels1;
+};
+
+TEST_F(VowelCountTest, getCountPass) {
+  ASSERT_TRUE(false);
+}
+
