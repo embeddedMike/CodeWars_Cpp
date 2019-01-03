506415311135b01ed13c098b6eb0288dc8818df6
Michal <209486.mb@gmail.com>
2019-01-03 21:13:10 +0100

    Modification of EvenNumbers.cpp file
    - Add further implementation of evenNumbersInVector method

--- src/EvenNumbers.cpp
+++ src/EvenNumbers.cpp
@@ -1,6 +1,11 @@
 #include "../inc/EvenNumbers.hpp"
 
 std::vector<int> EvenNumbers::evenNumbersInVector(std::vector<int> arr, int n) {
-  std::vector<int> result{0,1};
+  std::vector<int> result{};
+  for(int i = 0; i < n; i++){
+    for(std::vector<int>::reverse_iterator it = arr.rbegin(); it != arr.rend(); it++) {
+      result.push_back(*it);
+    }
+  }
   return result;
 }
