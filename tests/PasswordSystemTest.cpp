c45e927a0c767ed2397978e707c20d0134737e69
Michal <209486.mb@gmail.com>
2019-01-05 18:33:45 +0100

    Add PasswordSystemTest.cpp file
    - Add PasswordSystemTest class
    - Add test fixture checkDigits

---
+++ tests/PasswordSystemTest.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "../inc/PasswordSystem.hpp"
+
+struct PasswordSystemTest : public ::testing::Test {
+  PasswordSystem PassSysGtest;
+};
+
+TEST_F(PasswordSystemTest, checkDigits) {
+  std::string result = PassSysGtest.helpZoom({1,0,0,1,0,0,1});
+  ASSERT_STREQ("Yes",result.c_str());
+}
