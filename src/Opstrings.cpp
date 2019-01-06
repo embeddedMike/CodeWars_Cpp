85c5217da86472a21ce25f77f87c29d01679a67b
Michal <209486.mb@gmail.com>
2019-01-06 17:47:58 +0100

    Modification of Opstrings.cpp
    - Add source code inside method named vertMirror

--- src/Opstrings.cpp
+++ src/Opstrings.cpp
@@ -2,7 +2,21 @@
 
 std::string Opstrings::vertMirror(const std::string &strng) 
 {
-  return "";
+  std::string result("");
+  std::regex reg("[a-zA-Z0-9]+");
+  std::sregex_iterator currentMatch(strng.begin(),strng.end(),reg);
+  std::sregex_iterator lastMatch = std::sregex_iterator();
+  for(std::sregex_iterator i = currentMatch; i != lastMatch; ++i) 
+  {
+    std::smatch match = *i;
+    std::string match_str = match.str();
+    std::reverse(std::begin(match_str), std::end(match_str));
+    result += match_str;
+    result += '\n';
+    std::cout << result;
+  }
+  result.pop_back();
+  return result;
 }
 
 std::string Opstrings::horMirror(const std::string &strng) 
