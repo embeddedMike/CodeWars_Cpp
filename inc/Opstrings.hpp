eea6b6a3bebd64d61fdedc55810438f2817c1316
Michal <209486.mb@gmail.com>
2019-01-06 21:50:30 +0100

    Modification of Opstrings.hpp
    - Add template type deduction

--- inc/Opstrings.hpp
+++ inc/Opstrings.hpp
@@ -7,5 +7,8 @@ class Opstrings
   public:
     static std::string vertMirror(const std::string &strng);
     static std::string horMirror(const std::string &strng);
-    //static std::string oper(..., const std::string &strng);
+    template<typename FN>
+    static std::string oper(const FN& op, const std::string &strng) {
+      return op(strng);
+    }
 };
