2e3a9c4620aff444a63ad5103bcb9af0a9eeb3aa
Michal <209486.mb@gmail.com>
2019-01-06 16:31:27 +0100

    Add Opstrings.hpp file
    - Add of the Opstrings class
    - Add two static methods inside class

---
+++ inc/Opstrings.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+class Opstrings
+{
+  public:
+    static std::string vertMirror(const std::string &strng);
+    static std::string horMirror(const std::string &strng);
+    //static std::string oper(..., const std::string &strng);
+};
