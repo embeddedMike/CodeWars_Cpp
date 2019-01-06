48878cf07e98b5e7c59430d89cb21712b5c6b4ed
Michal <209486.mb@gmail.com>
2019-01-06 16:34:12 +0100

    Add Opstrings.cpp file
    - Add the vertMirror method definition
    - Add the horMirror method definition

---
+++ src/Opstrings.cpp
@@ -0,0 +1,11 @@
+#include "../inc/Opstrings.hpp"
+
+std::string Opstrings::vertMirror(const std::string &strng) 
+{
+  return "";
+}
+
+std::string Opstrings::horMirror(const std::string &strng) 
+{
+  return "";
+}
