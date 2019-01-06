b5cb1348d0ab030cd3157c003c5ca50e150a3029
Michal <209486.mb@gmail.com>
2019-01-06 22:01:35 +0100

    Modification of OpstringsTest.cpp
    - Add test fixtures for function template

--- tests/OpstringsTest.cpp
+++ tests/OpstringsTest.cpp
@@ -11,8 +11,20 @@ TEST_F(OpstringsTest, getValueFromVertMirror)
   ASSERT_STREQ("dcba\nhgfe\nlkji\nponm",result.c_str());
 }
 
+TEST_F(OpstringsTest, getValueFromOperPassVertMirror)
+{
+  std::string result = OpstringsGtest.oper(OpstringsGtest.vertMirror,"abcd\nefgh\nijkl\nmnop");
+  ASSERT_STREQ("dcba\nhgfe\nlkji\nponm",result.c_str());
+}
+
 TEST_F(OpstringsTest, getValueFromHorMirror)
 {
   std::string result = OpstringsGtest.horMirror("abcd\nefgh\nijkl\nmnop");
+  ASSERT_STREQ("mnop\nijkl\nefgh\nabcd",result.c_str());
+}
+
+TEST_F(OpstringsTest, getValueFromOperPassHorMirror)
+{
+  std::string result = OpstringsGtest.oper(OpstringsGtest.horMirror,"abcd\nefgh\nijkl\nmnop");
   ASSERT_STREQ("mnop\nijkl\nefgh\nabcd",result.c_str());
 }
