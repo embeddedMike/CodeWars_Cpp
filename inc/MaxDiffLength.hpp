ba44fc34f64515885a573b159ee81367b558c064
Michal <209486.mb@gmail.com>
2019-01-07 22:06:02 +0100

    Add MaxDiffLength.hpp file
    - Add class MaxDiffLength with mxdiflg static method

---
+++ inc/MaxDiffLength.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <algorithm>
+
+class MaxDiffLength
+{
+public:
+  static int mxdiflg(std::vector<std::string> &a1, std::vector<std::string> &a2);
+};
