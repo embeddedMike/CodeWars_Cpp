8d3b7ceff81e1426e4e096331af30e59c56dadc5
Michal <209486.mb@gmail.com>
2019-01-12 20:57:25 +0100

    Use clang-format for Opstrings.hpp

--- inc/Opstrings.hpp
+++ inc/Opstrings.hpp
@@ -1,14 +1,13 @@
 #pragma once
-#include <string>
-#include <regex>
 #include <iostream>
-class Opstrings
-{
-  public:
-    static std::string vertMirror(const std::string &strng);
-    static std::string horMirror(const std::string &strng);
-    template<typename FN>
-    static std::string oper(const FN& op, const std::string &strng) {
-      return op(strng);
-    }
+#include <regex>
+#include <string>
+class Opstrings {
+ public:
+  static std::string vertMirror(const std::string &strng);
+  static std::string horMirror(const std::string &strng);
+  template <typename FN>
+  static std::string oper(const FN &op, const std::string &strng) {
+    return op(strng);
+  }
 };
