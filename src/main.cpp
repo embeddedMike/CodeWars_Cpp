c06281f75ee964989bbd195ba337baba0dc6c53d
Michal <209486.mb@gmail.com>
2019-01-22 01:03:06 +0100

    Modification of main.cpp
    - Clear content inside main method
    - Remove header files

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,1 @@
-#include "../inc/TemplateExamples.hpp"
-//#include "../inc/Opstrings.hpp"
-//#include <chrono>
-//#include "../inc/main.hpp"
-//#include "../inc/CountDig.hpp"
-int main() {
-  for (int i = 0; i < 4; i++) {
-  }
-
-  /*print(1);
- Opstrings TestObject;
- std::cout << TestObject.vertMirror("abcd\nefgh\nijkl\nmnop") << std::endl;
- std::cout << TestObject.oper(TestObject.vertMirror,"abcd\nefgh\nijkl\nmnop")
- << std::endl;
- */
-}
+int main() {}
