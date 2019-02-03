9690e587b2a6168b020f5a44cca4f08930879ca2
Michal <209486.mb@gmail.com>
2019-02-03 16:54:14 +0100

    Add FormTheLargestTest.cpp file
    - Add struct named FormTheLargestTest
    - Add test fixture. Test fixture class name: FormTheLargestTest. Test
    case name: getValueFromMaxNumber

---
+++ tests/FormTheLargestTest.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "../inc/FormTheLargest.hpp"
+
+struct FormTheLargestTest : public ::testing::Test {
+  FormTheLargest LargestGtest;
+};
+
+TEST_F(FormTheLargestTest, getValueFromMaxNumber) {
+  long long int result = LargestGtest.maxNumber(234256);
+  ASSERT_TRUE(result == 0);
+}
