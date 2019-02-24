7a4b3f3941596b7be70c65d7bae4ae7e2b79acbd
Michal <209486.mb@gmail.com>
2019-02-25 00:17:20 +0100

    Modification of ScalingSquaredStrings.cpp
    - Add method definition named "scale"

---
+++ src/ScalingSquaredStrings.cpp
@@ -0,0 +1,5 @@
+#include "../inc/ScalingSquaredStrings.hpp"
+std::string ScalingSquaredStrings::scale(const std::string &strng, int k,
+                                         int n) {
+  return "";
+}
