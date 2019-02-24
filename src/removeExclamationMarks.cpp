65b80190149d286cb264667c915e7e7196adb319
Michal <209486.mb@gmail.com>
2019-02-24 22:47:04 +0100

    Modfication in removeExclamationMarks.cpp
    - Add final solution for code wars platform task

--- src/removeExclamationMarks.cpp
+++ src/removeExclamationMarks.cpp
@@ -1,6 +1,12 @@
 #include "../inc/removeExclamationMarks.hpp"
 
 std::string RemoveExclamationMarks::removeExlamationMarksProcedure(
-    std::string) {
-  return "";
+    std::string str) {
+  std::string result("");
+  for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
+    if (*it != '!') {
+      result += *it;
+    }
+  }
+  return result;
 }
