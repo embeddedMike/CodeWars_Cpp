14262d505bb554ff7a795d032796d21ed3c4ef61
Michal <209486.mb@gmail.com>
2019-02-25 04:21:36 +0100

    Modification of ScalingSquaredStrings.hpp
    - Add method declaration named "horizontalScale"
    - Add method declaration named "verticalScale"

--- inc/ScalingSquaredStrings.hpp
+++ inc/ScalingSquaredStrings.hpp
@@ -2,5 +2,7 @@
 #include <string>
 class ScalingSquaredStrings {
  public:
-  static std::string scale(const std::string &strng, int k, int n);
+  static std::string horizontalScale(const std::string &strng, int k);
+  static std::string verticalScale(const std::string &strng, int n);
+  static std::string finalScale(const std::string &strng, int k, int n);
 };
