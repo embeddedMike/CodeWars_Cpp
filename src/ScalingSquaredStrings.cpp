0fb2dfdecf1440c13bc7c430371cef0f3413b8bf
Michal <209486.mb@gmail.com>
2019-02-25 04:28:12 +0100

    Modification of ScalingSquaredStrings.cpp
    - Add implementation of "horizontalScale" method
    - Add implementation of "verticalScale" method

--- src/ScalingSquaredStrings.cpp
+++ src/ScalingSquaredStrings.cpp
@@ -1,28 +1,39 @@
 #include "../inc/ScalingSquaredStrings.hpp"
-std::string ScalingSquaredStrings::scale(const std::string &strng, int k,
-                                         int n) {
-  std::string result("");
-  std::string finalResult("");
-  std::regex reg("[^\\n][a-zA-Z0-9]+");
+
+std::string ScalingSquaredStrings::horizontalScale(const std::string &strng,
+                                                   int k) {
+  std::string partialResult("");
   for (std::string::const_iterator it = strng.begin(); it != strng.end();
        ++it) {
     if (*it != '\n') {
       for (int i = 0; i < k; i++) {
-        result += (*it);
+        partialResult += (*it);
       }
     } else {
-      result += '\n';
+      partialResult += '\n';
     }
   }
-  std::sregex_iterator currentMatch(result.begin(), result.end(), reg);
+  return partialResult;
+}
+std::string ScalingSquaredStrings::verticalScale(const std::string &strng,
+                                                 int n) {
+  std::string partialResult("");
+  std::regex reg("[^\\n][a-zA-Z0-9]+");
+  std::sregex_iterator currentMatch(strng.begin(), strng.end(), reg);
   std::sregex_iterator lastMatch = std::sregex_iterator();
   for (std::sregex_iterator i = currentMatch; i != lastMatch; ++i) {
     std::smatch match = *i;
     for (int i = 0; i < n; i++) {
-      finalResult += match.str();
-      finalResult += '\n';
+      partialResult += match.str();
+      partialResult += '\n';
     }
   }
-  finalResult.pop_back();
-  return finalResult;
+  partialResult.pop_back();
+  return partialResult;
+}
+std::string ScalingSquaredStrings::finalScale(const std::string &strng, int k,
+                                              int n) {
+  std::string finalResult("");
+  finalResult = horizontalScale(strng, k);
+  return verticalScale(finalResult, n);
 }
