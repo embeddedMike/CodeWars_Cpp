e716c76608332709d8618ed2d91fac18d5b46818
Michal <209486.mb@gmail.com>
2019-04-29 19:45:48 +0200

    Revert "[task] Small changes in FormTheLargestTest.cpp"
    This reverts commit 4103a5a471d53cc3d52d7559432ae7183ab276f6.

--- tests/FormTheLargestTest.cpp
+++ tests/FormTheLargestTest.cpp
@@ -2,10 +2,10 @@
 #include "../inc/FormTheLargest.hpp"
 
 struct FormTheLargestTest : public ::testing::Test {
-  FormTheLargest LargestUnitTest;
+  FormTheLargest LargestGtest;
 };
 
 TEST_F(FormTheLargestTest, getValueFromMaxNumber) {
-  long long int result = LargestUnitTest.maxNumber(234256);
+  long long int result = LargestGtest.maxNumber(234256);
   ASSERT_TRUE(result == 0);
 }
