885c3dcdd23d10db6116ba0f2ab5905d3b49b971
Michal <209486.mb@gmail.com>
2019-09-22 22:13:13 +0200

    [task] Final solution for Kata

--- inc/SortOutTheManFromBoys.hpp
+++ inc/SortOutTheManFromBoys.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include <algorithm>
 #include <functional>
+#include <iostream>
 #include <iterator>
 #include <vector>
 class SortOutTheManFromBoys {
  public:
   std::vector<int> menFromBoys(std::vector<int> values);
-  std::vector<int> ascendingVectorForEvenNumbers(std::vector<int> values);
-  std::vector<int> descendingVectorForOddNumbers(std::vector<int> values);
+  std::vector<int> ascendingVectorForEvenNumbers(std::vector<int> &values);
+  std::vector<int> descendingVectorForOddNumbers(std::vector<int> &values);
 };
--- src/SortOutTheManFromBoys.cpp
+++ src/SortOutTheManFromBoys.cpp
@@ -2,23 +2,36 @@
 
 std::vector<int> SortOutTheManFromBoys::menFromBoys(std::vector<int> values) {
   std::vector<int> result{};
-  for (std::vector<int>::iterator it = values.begin(); it != values.end();) {
+  std::vector<int> ascendingVector{};
+  std::vector<int> descendingVector{};
+  for (std::vector<int>::iterator it = values.begin(); it != values.end();
+       ++it) {
     if (*it % 2 == 0) {
-      result.push_back(*it);
-      it = values.erase(it);
+      ascendingVector.push_back(*it);
     } else {
-      ++it;
+      descendingVector.push_back(*it);
     }
   }
-  // std::copy(values.begin(), values.end(), std::back_inserter(result));
+  ascendingVectorForEvenNumbers(ascendingVector);
+  descendingVectorForOddNumbers(descendingVector);
+  std::move(ascendingVector.begin(), ascendingVector.end(),
+            std::back_inserter(result));
+  std::move(descendingVector.begin(), descendingVector.end(),
+            std::back_inserter(result));
   return result;
 }
 
 std::vector<int> SortOutTheManFromBoys::ascendingVectorForEvenNumbers(
-    std::vector<int> values) {
+    std::vector<int> &values) {
+  std::sort(values.begin(), values.end(), std::less<int>());
+  auto last = std::unique(values.begin(), values.end());
+  values.erase(last, values.end());
   return values;
 }
 std::vector<int> SortOutTheManFromBoys::descendingVectorForOddNumbers(
-    std::vector<int> values) {
-  return {};
+    std::vector<int> &values) {
+  std::sort(values.begin(), values.end(), std::greater<int>());
+  auto last = std::unique(values.begin(), values.end());
+  values.erase(last, values.end());
+  return values;
 }
